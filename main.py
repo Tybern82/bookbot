5039328fe6fdd1da67cc25f47c9ab9ca0031e4f6
Tybern82 <jeff@tybern.com>
2025-09-25 21:25:57 +1000

    ch2.3: initial file read

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text (filename):
+    with open(filename) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main ():
+    fkpath = "books/frankenstein.txt"
+    fktext = get_book_text(fkpath)
+    print(fktext)
+
+main()

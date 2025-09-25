6d3734cae5e7c8ea4dd36137ca71ed32cb8972ff
Tybern82 <jeff@tybern.com>
2025-09-25 21:28:47 +1000

    ch2.4: count words

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ def get_book_text (filename):
     with open(filename) as f:
         file_contents = f.read()
         return file_contents
+    
+def count_words(text):
+    return len(text.split())
 
 def main ():
     fkpath = "books/frankenstein.txt"
     fktext = get_book_text(fkpath)
-    print(fktext)
+    num_words = count_words(fktext)
+    print(f"Found {num_words} total words")
 
 main()
